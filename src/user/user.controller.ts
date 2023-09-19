504263de077e3d277a9ab2c78098ec9072242d2a
seokimun <gimun08@naver.com>
2023-09-19 23:32:14 +0900

    fuck

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { User } from 'src/user.entity';
 
 @Controller('user')
 export class UserController {
     constructor (private userService: UserService) {}
-
-    @Get
-    
 }
