31e4b2a55013dc501392f113a127bca431916fb4
seokimun <gimun08@naver.com>
2023-09-26 15:48:06 +0900

    ?

--- src/user.entity.ts
+++ src/user.entity.ts
@@ -1,14 +1,14 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
-
+import { ASYNC_METHOD_SUFFIX } from '@nestjs/common/module-utils/constants';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
-    @PrimaryGeneratedColumn()
-    id : number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    Name: string;
+  @Column()
+  Name: string;
 
-    @Column({ default: true })
-    isActive: boolean;
+  @Column({ default: true })
+  isActive: boolean;
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -5,38 +5,35 @@ import { Repository } from 'typeorm/repository/Repository';
 
 @Injectable()
 export class UserService {
-    constructor(
-        @InjectRepository(User)
-        private userRepository: Repository<User>,
-    ) {
-        this.userRepository = userRepository
-    }
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {
+    this.userRepository = userRepository;
+  }
 
-    // User 리스트 조회
-    findAll(): Promise<User[]>{
-        return this.userRepository.find();
-    }
+  // User 리스트 조회
+  findAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
 
-    // 특정유저 조회
-    findOne(id: number): Promise<User>{
-        return this.userRepository.findOneBy({id});
-    }
+  // 특정유저 조회
+  findOne(id: number): Promise<User> {
+    return this.userRepository.findOneBy({ id });
+  }
 
-    // 유저저장
-    async saveUser(user: User): Promise<void> {
-        await this.userRepository.save(user);
-    }
+  // 유저저장
+  async saveUser(user: User): Promise<void> {
+    await this.userRepository.save(user);
+  }
 
-    // 유저수정
-    async updateUser(id: number, user: User): Promise<void> {
-        await this. userRepository.update(
-        id,
-        user
-        );
-    }
+  // 유저수정
+  async updateUser(id: number, user: User): Promise<void> {
+    await this.userRepository.update(id, user);
+  }
 
-    // 유저삭제
-    async deleteUser(id: number): Promise<void> {
-        await this.userRepository.delete(id);
-    }
+  // 유저삭제
+  async deleteUser(id: number): Promise<void> {
+    await this.userRepository.delete(id);
+  }
 }
